149d19e98ab11101f94f278c70e85dc2ba251dde
Tetyana-Stepanova <95179655+Tetyana-Stepanova@users.noreply.github.com>
2022-02-08 14:40:11 +0200

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -1,15 +1,25 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Numbers {
     public static void main(String[] args) {
-        String[] numbersArray = {"1, 2, 0","4, 5"};
-        String[] result = Stream.concat(
+        String[] numbersArray = {"1, 12, 676, 0, 25","4, 15"};
+        List<Integer> result = Stream.concat(
                 Arrays.stream(numbersArray[0].split(", ")),
                 Arrays.stream(numbersArray[1].split(", "))
         )
+                .map(Integer::valueOf)
                 .sorted()
-                .toArray(String[]::new);
-        System.out.print(Arrays.toString(result));
+                .collect(Collectors.toList());
+        for (int i = 0; i < result.size(); i++) {
+            if( i == result.size()-1){
+                System.out.print(result.get(i));
+            }
+            else{
+                System.out.print(result.get(i)+", ");
+            }
+        }
     }
 }
